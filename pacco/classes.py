58d8777e2178da4dd137e587ee746da6b7a0a841
Kevin Winata <kevinwinatamichael@gmail.com>
2019-10-10 11:22:42 +0800

    Move client to nexus_interfaces.py

--- pacco/classes.py
+++ pacco/classes.py
@@ -1,35 +1,11 @@
 import hashlib
 import json
 import logging
-from typing import Dict, List, Any, Optional, Tuple
+from typing import Dict, List, Any, Optional
+
+from pacco.nexus_interfaces import Client
 
 ALLOWED_STRINGS_FILE_NAME = "allowed_settings.txt"
-
-
-class Client:
-    """
-    The client shall act like git as in only index files and not directory.
-    """
-    def if_file_exists(self, file_path: List[str]) -> bool:
-        return True
-
-    def get_file_content(self, file_path: List[str]) -> str:
-        return 'hello world!'
-
-    def write_file(self, content: str, file_path: List[str]) -> None:
-        return
-
-    def lsr_dir(self, dir_path: List[str]) -> List[Tuple[bool, str]]:
-        return []
-
-    def rm_dir(self, dir_path: List[str]) -> None:
-        """
-        rm_dir here means remove recursively all files within the directory
-        """
-        return
-
-    def change_directory(self, dir_path: List[str]) -> None:
-        return
 
 
 class PackageRegistry:
