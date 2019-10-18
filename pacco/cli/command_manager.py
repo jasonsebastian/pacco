6616bbdc2b4eb3cdf0fb4b1f3b231c7c046b79ce
Jason Sebastian <jason.sebastian11@gmail.com>
2019-10-18 13:46:54 +0800

    Fix bug on unpacking arguments

--- pacco/cli/command_manager.py
+++ pacco/cli/command_manager.py
@@ -15,27 +15,25 @@ class CommandManager:
         """
         Entry point for executing commands, dispatcher to class methods.
         """
-        try:
-            command = args[0]
-            remaining_args = args[1:]
-            commands = self.__get_commands()
-            method = commands[command]
-            method(remaining_args)
-        except KeyError as exc:
+        if not args:
+            self.__show_help()
+            return
+        command = args[0]
+        remaining_args = args[1:]
+        commands = self.__get_commands()
+        if command not in commands:
             if command in ["-h", "--help"]:
                 self.__show_help()
                 return
-            if command == "remote":
-                Remote(self.__pacco).run(*remaining_args)
-                return
-            self.__out.writeln("{} is not a pacco command. See 'pacco --help'.".format(str(exc)), error=True)
-        except IndexError:  # commands specified
-            self.__show_help()
+            self.__out.writeln("'pacco {}' is an invalid command. See 'pacco --help'.".format(command), error=True)
+            return
+        method = commands[command]
+        method(*remaining_args)
 
     def __get_commands(self) -> Dict[str, Callable]:
         result = {}
         for method_name, method in inspect.getmembers(self, predicate=inspect.ismethod):
-            if not method_name.startswith('_') and method_name not in ["run", "remote"]:
+            if not method_name.startswith('_') and method_name not in ["run"]:
                 result[method_name] = method
         return result
 
@@ -46,12 +44,15 @@ class CommandManager:
         for name in commands:
             appended_name = "pacco {}".format(name)
             print(fmt % appended_name, end="")
-            docstring_lines = commands[name].__doc__.split('\n')
-            data = []
-            for line in docstring_lines:
-                line = line.strip()
-                data.append(line)
-            self.__out.writeln(' '.join(data))
+            if commands[name].__doc__:
+                docstring_lines = commands[name].__doc__.split('\n')
+                data = []
+                for line in docstring_lines:
+                    line = line.strip()
+                    data.append(line)
+                self.__out.writeln(' '.join(data))
+            else:
+                self.__out.writeln("")  # Empty docs
         self.__out.writeln("")
         self.__out.writeln("Pacco commands. Type 'pacco <command> -h' for help")
 
@@ -63,12 +64,14 @@ class CommandManager:
         parser.add_argument("registry", help="which registry to download")
         parser.add_argument("path", help="path to download registry")
         parser.add_argument("settings", nargs="+", help="settings for the specified registry")
-        args = parser.parse_args(*args)
+        args = parser.parse_args(args)
         self.__pacco.download(args.registry, args.path, *args.settings)
+
+    def remote(self, *args: str):
+        Remote(self.__pacco).run(*args)
 
 
 class Remote:
-
     def __init__(self, pacco_api):
         self.__pacco = pacco_api
         self.__out = pacco_api.out
@@ -78,20 +81,21 @@ class Remote:
         """
         Entry point for executing commands, dispatcher to class methods.
         """
-        try:
-            command = args[0]
-            commands = self.__get_commands()
-            method = commands[command]
-            remaining_args = args[1:]
-            method(remaining_args)
-        except KeyError as exc:
+        if not args:
+            self.__show_help()
+            return
+        command = args[0]
+        remaining_args = args[1:]
+        commands = self.__get_commands()
+        if command not in commands:
             if command in ["-h", "--help"]:
                 self.__show_help()
                 return
-            self.__out.writeln("pacco remote {} is not a pacco command. See 'pacco remote --help'.".format(str(exc)),
+            self.__out.writeln("'pacco remote {}' is an invalid command. See 'pacco remote --help'.".format(command),
                                error=True)
-        except IndexError:  # commands specified
-            self.__show_help()
+            return
+        method = commands[command]
+        method(*remaining_args)
 
     def __get_commands(self) -> Dict[str, Callable]:
         result = {}
@@ -107,12 +111,15 @@ class Remote:
         for name in commands:
             appended_name = "pacco remote {}".format(name)
             print(fmt % appended_name, end="")
-            docstring_lines = commands[name].__doc__.split('\n')
-            data = []
-            for line in docstring_lines:
-                line = line.strip()
-                data.append(line)
-            self.__out.writeln(' '.join(data))
+            if commands[name].__doc__:
+                docstring_lines = commands[name].__doc__.split('\n')
+                data = []
+                for line in docstring_lines:
+                    line = line.strip()
+                    data.append(line)
+                self.__out.writeln(' '.join(data))
+            else:
+                self.__out.writeln("")  # Empty docs
         self.__out.writeln("")
         self.__out.writeln("Pacco remote commands. Type 'pacco remote <command> -h' for help")
 
@@ -121,7 +128,7 @@ class Remote:
         List existing remotes.
         """
         parser = argparse.ArgumentParser(prog="pacco remote list")
-        parser.parse_args(*args)
+        parser.parse_args(args)
         remotes = self.__rm.list_remote()
         if not remotes:
             self.__out.writeln("No registered remotes")
@@ -135,7 +142,7 @@ class Remote:
         parser = argparse.ArgumentParser(prog="pacco remote add")
         parser.add_argument("name", help="remote name")
         parser.add_argument("type", help="remote type", choices=ALLOWED_REMOTE_TYPES)
-        args = parser.parse_args(*args)
+        args = parser.parse_args(args)
         if args.type == "local":
             path = input("Path (if empty, ~/.pacco/ will be used): ")
             self.__rm.add_remote(args.name, {
@@ -160,7 +167,7 @@ class Remote:
         """
         parser = argparse.ArgumentParser(prog="pacco remote remove")
         parser.add_argument("name", help="remote name")
-        args = parser.parse_args(*args)
+        args = parser.parse_args(args)
         self.__rm.delete_remote(args.name)
 
 
